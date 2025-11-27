b06ca0cfde04e8c239057c6ec35d609e23185233
Emeç Yıldız <emecyildiz@posta.mu.edu.tr>
2025-11-27 14:45:03 +0300

    eski class kaldırıldı arayüz eklendi ve arayüz ile tarama kodları birleşik tek class ta yazıldı

--- networkshark/src/porthunter.java
+++ networkshark/src/porthunter.java
@@ -1,10 +1,46 @@
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStreamReader;
 
+public class porthunter extends JFrame {
+    DefaultTableModel model;
+    JButton btntara;
+    JTable table;
 
-public class rawdata {
+
+    public porthunter(){
+        String[] kolonlar = {"PID", "Program Adı", "Protokol", "Adres/Port", "Durum"};
+        model = new DefaultTableModel(kolonlar,0);
+        btntara = new JButton("taramayı başlat");
+        setSize(800,600);
+
+        setTitle("first try");
+
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        int w = getSize().width;
+        int h = getSize().height;
+        int x  = (dim.width - w)/2;
+        int y = (dim.height - h)/2;
+        setLocation(x,y);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+
+        table = new JTable(model);
+        JScrollPane scroller = new JScrollPane(table);
+        add(scroller, BorderLayout.CENTER);
+        add(btntara, BorderLayout.NORTH);
+        btntara.addActionListener(e -> scanner());
+        setVisible(true);
+    }
     public static void main(String[] args){
+        porthunter porthunt = new porthunter();
+
+    }
+    public void scanner(){
+
         try {
             String[]  commends = {"cmd","/c","netstat -ano"};
             ProcessBuilder builder = new ProcessBuilder(commends);
@@ -16,6 +52,7 @@ public class rawdata {
             String line = null;
             while ((line = bufferedReader.readLine()) != null){
                 if (line.contains("LISTENING")){
+
                     String[] pieces = line.trim().split("\\s+");
                     String pid = pieces[pieces.length-1];
                     String[] tasklist = {"cmd","/c","tasklist /fi \"PID eq "+pid+"\""};
@@ -25,17 +62,23 @@ public class rawdata {
                     Process process1 = builder1.start();
                     BufferedReader bufferedReader1 = new BufferedReader(new InputStreamReader(process1.getInputStream()));
                     String line1 = null;
+                    String rawadress = pieces[1];
+                    int splitindex = rawadress.lastIndexOf(":");
+                    String ip = rawadress.substring(0,splitindex);
+                    String port = rawadress.substring(splitindex+1);
                     while ((line1 = bufferedReader1.readLine()) != null){
                         if(line1.isEmpty() || line1.startsWith("Image Name") || line1.startsWith("=")){
                             continue;
                         }
-                       String[] pieces1 = line1.trim().split("\\s+");
-                       String exe = pieces1[0];
-                       System.out.println("Port:  " +pieces[1]+ "  <---> Program:  "+exe+ " <---> PID:  " +pieces1[1]);
+                        String[] pieces1 = line1.trim().split("\\s+");
+
+
+                        String exe = pieces1[0];
+                        model.addRow(new Object[]{pid, exe, pieces[0],port,pieces[3]});
 
                     }
 
-            }   }
+                }   }
             bufferedReader.close();
         }
         catch (Exception e){
